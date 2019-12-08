8923ea922145110a636da760fbc42ce3f77acced
gebhartn <nicholas.gebhart@gmail.com>
2019-12-07 18:21:27 -0800

    nothing to see here

---
+++ src/components/LinkList.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Link from "./Link";
+import { useQuery } from "urql";
+import gql from "graphql-tag";
+
+const FEED_QUERY = gql`
+  query FeedQuery($first: Int, $skip: Int, $orderBy: LinkOrderByInput) {
+    feed(first: $first, skip: $skip, orderBy: $orderBy) {
+      count
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
+
+const LinkList = props => {
+  const isNewPage = props.location.pathname.includes("new");
+  const page = parseInt(props.match.params.page, 10);
+  const pageIndex = isNewPage ? (page - 1) * 10 : 0;
+
+  const variables = React.useMemo(
+    () => ({
+      skip: isNewPage ? (page - 1) * 10 : 0,
+      first: isNewPage ? 10 : 100,
+      orderBy: isNewPage ? "createdAt_DESC" : null
+    }),
+    [isNewPage, page]
+  );
+
+  const [result] = useQuery({ query: FEED_QUERY, variables });
+  const { data, fetching, error } = result;
+
+  if (fetching) return <div>Fetching</div>;
+  if (error) return <div>Error!</div>;
+
+  const linksToRender = data.feed.links;
+
+  return (
+    <div>
+      {linksToRender.map((link, index) => (
+        <Link key={link.id} link={link} index={pageIndex + index} />
+      ))}
+    </div>
+  );
+};
+
+export default LinkList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { getToken } from "./token";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider, Client, dedupExchange, fetchExchange } from "urql";
+import { cacheExchange } from "@urql/exchange-graphcache";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const cache = cacheExchange({});
+
+const client = new Client({
+  url: `http://localhost:4000`,
+  fetchOptions: () => {
+    const token = getToken();
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : "" }
+    };
+  },
+  exchanges: [dedupExchange, cache, fetchExchange]
+});
+
+render(
+  <Router>
+    <Provider value={client}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
+);
